8a35086ee1eaa11c9b930d5da465ac97f6b94cd1
gregyon <gregoire.bonin@sfr.fr>
2025-02-16 01:12:21 +0000

    Déploiement automatique Fly.io

---
+++ covoiturage/src/Entity/Incident.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IncidentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: IncidentRepository::class)]
+class Incident
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
